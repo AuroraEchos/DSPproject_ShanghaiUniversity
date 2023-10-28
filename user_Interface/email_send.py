c265babd0d03431d9d639d6d743ceee2399ae752
Wenhao Liu <3111013462@qq.com>
2023-10-28 13:10:05 +0800

    更新 email_send.py

--- user_Interface/email_send.py
+++ user_Interface/email_send.py
@@ -2,8 +2,8 @@ import yagmail
 
 def send_email(user_email,user_feedback):
     try:
-        yag=yagmail.SMTP(user='2726433780@qq.com',password='wycjjftbtgqydffe',host='smtp.qq.com')
-        yag.send(to=['3111013462@qq.com'],subject=user_email,contents=user_feedback)
+        yag=yagmail.SMTP(user='',password='',host='')
+        yag.send(to=[''],subject=user_email,contents=user_feedback)
         print('Email send success')
     except:
         print('Email send fail')
